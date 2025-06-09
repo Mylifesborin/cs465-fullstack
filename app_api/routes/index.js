4d60fde73783b4d5b4a5bcc0913d4bac2aabb072
Yakuba Conde <kubaconde123@gmail.com>
2025-06-09 01:19:06 -0400

    Module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const tripsController = require("../controllers/trips");
+
+// define route for our trips endpoint
+router.route("/trips").get(tripsController.tripsList); //GET Method routes tripList
+
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+
+module.exports = router;
